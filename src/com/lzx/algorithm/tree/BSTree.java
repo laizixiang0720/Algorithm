2f6f59e6147b6fe82b440a670c14821579e42871
來紫祥 <laizixiang@sinosoft.hk>
2022-04-14 16:36:06 +0800

    初始项目

---
+++ src/com/lzx/algorithm/tree/BSTree.java
@@ -0,0 +1,43 @@
+package com.lzx.algorithm.tree;
+
+class Node {
+  private int data;
+  private Node right;
+  private Node left;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+}
+
+/**
+ * @author laizixiang
+ */
+public class BSTree {
+
+    BSTree(int[] datas){
+        for (int data : datas) {
+
+        }
+    }
+}
